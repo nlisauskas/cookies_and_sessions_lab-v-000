d87e04ce59a87229b9ac0b7f4e9993efc89ab6a1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 01:50:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
+    if session[:cart]
+      session[:cart]
+    else
     session[:cart] = []
+  end
   end
 end
